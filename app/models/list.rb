82f4d6c7793f1fe2f4fbb4c138fec3307996a186
Ismael Sentissi <ismael.sentissi@gmail.com>
2023-11-17 14:01:27 +0100

    Cloudinary

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,9 @@
 class List < ApplicationRecord
   has_many :bookmarks
   has_many :movies, through: :bookmarks, dependent: :destroy
+  has_one_attached :photo
 
   validates :name, presence: true, uniqueness: true
-  validates :image_url, presence: true
+  validates :photo, content_type: { in: ['image/png', 'image/jpg', 'image/jpeg'], message: 'must be a valid image format' }
+  validates :photo, size: { less_than: 5.megabytes, message: 'should be less than 5MB' }
 end
